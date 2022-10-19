b999837cd7d8e03da518f5bdd96a5f3c0c8551f1
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2022-10-19 11:42:38 +0200

    fecha_hora
    una practica

---
+++ practicas/fecha_hora.py
@@ -0,0 +1,36 @@
+
+
+class Time:
+
+    def __init__(self,hh=0,mm=0,ss=0):
+        self.hh = hh
+        self.mm = mm
+        self.ss = ss
+
+   
+   
+    def __str__(self):        
+        return '%02d:%02d:%02d' % (self.hh,self.mm,self.ss)
+
+
+   
+
+class Date:
+
+    def __init__(self, dd,mm,yy):
+        self.dd = dd
+        self.mm = mm
+        self.yy = yy
+
+    def __str__(self):
+        return "%02d/%02d/%04d" % (self.dd,self.mm,self.yy)
+
+    def esBisiesto(self):
+        anyo = self.__yy    
+        if  (anyo % 4 == 0 and anyo % 100 != 0) or (anyo%100 == 0 and anyo % 400 == 0):
+            return True    
+        else:
+            return False
+
+ 
+
