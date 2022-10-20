dd00d09851df48c2dd20b35439a06fbc3be3bc21
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2022-10-20 10:12:09 +0200

    jueves2
    código

---
+++ codigo 1/consola_format.py
@@ -0,0 +1,26 @@
+"""
+Salida formateada por consola
+"""
+
+def imprimir(cab, d, f=None):
+    n = max([len(s) for s,_ in d])
+    n1 = len(cab[1])
+    fcab = f"%-{n}s\t%{n1}s\t%{n1}s\t%{n1}s"
+    print(fcab % cab)  
+    fdet = f"%-{n}s\t%{n1}.2f\t%{n1}.2f\t%{n1}.2f"
+    for prod, importe in d:
+        iva = round(importe * 0.21)
+        total = importe + iva       
+        print(fdet % (prod, importe, iva, total))
+        print
+
+if __name__ == '__main__':
+    d = [("Ordenador Sobremesa",660.5),\
+    ("Teclado",45.56), \
+        ("Impresora",124.3), \
+            ("Tableta",223.7)]
+
+    cab = ('PRODUCTO','IMPORTE','IVA','TOTAL') 
+    f = open('consola_format.txt',"w")      
+    imprimir(cab, d) 
+    f.close()
