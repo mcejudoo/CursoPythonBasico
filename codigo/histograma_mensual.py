854ed740db4f24c40ed10d32214751d14d5243fd
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-01-11 10:51:52 +0100

    miercoles_10_51

---
+++ codigo/histograma_mensual.py
@@ -0,0 +1,30 @@
+"""
+Generar datos aleatorios de una fecha del 2022 con su ingreso.
+Calcular un histograma de ingresos mensuales y ordenar desc. por mes
+"""
+from datetime import date
+from random import randint
+
+def generarDatos(año=2022, n=100):
+    """
+    Genera una lista de tuplas indicando la fecha y el ingreso.
+    """
+    L = [(date(año, randint(1,12), randint(1,28)), randint(1000, 10000)) for _ in range(n)]
+    return L
+
+def histogramaMes(L):
+    """
+    Histograma mensual de ingresos
+    """
+    histo = dict()
+
+    # imprimir el mes y el ingreso
+
+if __name__ == '__main__':
+    L = generarDatos()
+    print(L[:5])
+
+    histogramaMes(L)
+
+
+
