febed9161ade151ff00bd4c433001480cbd32b93
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-01-12 10:22:41 +0100

    Jueves_11_22

--- codigo/ecuacion2.py
+++ codigo/ecuacion2.py
@@ -11,10 +11,22 @@ import math
 # Convertirlo en una función y lanzar excepciones cuando no hay solución
 # Capturar el error e imprimirlo en el código principal
 
-# Inicialización múltiple:
-a,b,c = 1,5,4 #1,2,3 #1,5,4
+def ecuacionGrado2(a,b,c):
+    x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+    x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
+    return x1, x2
 
-x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
-x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
-print(x1, x2)
+if __name__ == '__main__':
+    # Inicialización múltiple:
+    a,b,c = 1,2,3 #1,5,4
+    try:
+        x1,x2 = ecuacionGrado2(a,b,c)
+        print('x1', x1)
+        print('x2', x2)
+
+    except Exception as e:
+        print("error:", e)
+        print('No hay solución')
+
+
 
