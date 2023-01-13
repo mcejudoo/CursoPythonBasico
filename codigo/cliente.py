b9951af0a4b5100878b0bd6da604065ff6af1f26
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-01-13 13:58:26 +0100

    viernes_13_58

--- codigo/cliente.py
+++ codigo/cliente.py
@@ -1,15 +1,24 @@
 
 import socket 
 
-s = socket.socket() 
-s.connect(("localhost", 9999)) 
-while True: 
-    mensaje = input("> ") 
-    s.send(mensaje.encode('utf-8')) 
-    recibido = s.recv(1024) 
-    recibido=recibido.decode('utf-8')
+s = None
+try:
+    s = socket.socket() 
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+    s.connect(("localhost", 9999)) 
+    print('Conectado con el servidor ...')
+    while True: 
+        mensaje = input("> ") 
+        s.send(mensaje.encode('utf-8')) 
+        recibido = s.recv(1024) 
+        recibido=recibido.decode('utf-8')
 
-    if mensaje == "quit": break 
-    print("Recibido:", recibido )
-print("adios" )
-s.close() 
+        if mensaje == "quit": break 
+        print("Recibido:", recibido )
+    print("adios" )
+
+except Exception as e:
+    print('ERROR: ', e)
+    
+finally:
+    if s: s.close() 
--- codigo/servidor.py
+++ codigo/servidor.py
@@ -1,17 +1,26 @@
-
 import socket
 
-s = socket.socket()
-s.bind(("localhost", 9999))
-s.listen(1)
+s = None
+sc = None
+try:
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+    s.bind(("localhost", 9999))
+    s.listen(1)
+    print('Servidor conectado, a la espera de clientes ...')
+    sc, addr= s.accept()
+    print('Cliente conectado: ', addr)
+    while True: 
+        recibido = sc.recv(1024) 
+        recibido=recibido.decode('utf-8')
+        if recibido == "quit": break 
+        print("Recibido:", recibido) 
+        sc.send(recibido.encode('utf-8'))
+    print("fin comunicación")
+    
+except Exception as e:
+    print('ERROR: ', e)
 
-sc, addr= s.accept()
-while True: 
-    recibido = sc.recv(1024) 
-    recibido=recibido.decode('utf-8')
-    if recibido == "quit": break 
-    print("Recibido:", recibido) 
-    sc.send(recibido.encode('utf-8'))
-print("fin comunicación")
-sc.close()
-s.close()
+finally:    
+    if sc: sc.close()
+    if s: s.close()
