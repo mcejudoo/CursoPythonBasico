73c2dd8efae710cb68d272c51ca6086d01e0d2b2
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-02-06 11:51:36 +0100

    lunes_11_51

--- codigo_feb_1/operadores.py
+++ codigo_feb_1/operadores.py
@@ -21,3 +21,37 @@ print('Div real: ', n1 / n2)
 print('Div. entera: ', n1 // n2)
 print('módulo: ', n1 % n2)
 print('Raiz de 25: ', math.sqrt(25))
+
+# Operadores relacionales y lógicos:
+n = 23
+ini = 10
+fin = 50
+
+# Comprobar si n está dentro del intervalo [ini, fin]
+if n >= ini and n <= fin:
+    print('n en el intervalo')
+
+if ini <= n <= fin:
+    print('n en el intervalo')
+    
+# Comprobar que no está en el intervalo:
+n = 120
+if not (ini <= n <= fin):
+    print('n no esta en el intervalo')
+
+if n < ini or n > fin:
+    print('n no esta en el intervalo')
+
+# En python "" 0 None son False
+n = 0
+if not n:
+    print('Es cero')
+
+s = ''
+if not s:
+    print('Cadena vacía')
+
+p = None
+print(type(p))
+if not p:
+    print('Es None')
