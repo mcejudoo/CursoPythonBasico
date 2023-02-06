1aa9d75a1db316e0fc4f9e6e4b1a46856edc16c9
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-02-06 11:04:36 +0100

    lunes_11_04

---
+++ codigo_feb_1/operadores.py
@@ -0,0 +1,23 @@
+"""
+Pruebas con operadores:
+De cadena
+Aritméticos
+Relacionales y lógicos
+"""
+import math
+
+# De cadena:
+cad1 = "hola"
+cad2 = "adios"
+print(cad1 + cad2)
+print(cad1 * 10)
+
+# Aritméticos con números:
+n1 = 10
+n2 = 3
+
+print('potencia: 10**3: ', n1**n2)
+print('Div real: ', n1 / n2)
+print('Div. entera: ', n1 // n2)
+print('módulo: ', n1 % n2)
+print('Raiz de 25: ', math.sqrt(25))
