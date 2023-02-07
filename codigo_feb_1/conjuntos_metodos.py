6092eb75b916f72f350552d69686ef05fc6f192a
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-02-07 14:11:31 +0100

    martes_14_11

---
+++ codigo_feb_1/conjuntos_metodos.py
@@ -0,0 +1,29 @@
+"""
+Métodos sobre conjuntos
+"""
+
+c = {1,4,4,3,3,3,2,6}
+print(c,type(c))
+
+c2 = set([5,7,8,9,10,11,12,13,14,15])
+print(c2, type(c2))
+
+c1 = {1,2,3,4}
+c2 = {3,4,5,6}
+print(c1 - c2)
+print(c1 & c2)
+print(c1 ^ c2)
+print(c1 | c2)
+
+comida = ['Ana','Alberto','Nuria','Gema','Andres']
+cena = ['Ana', 'Andres', 'Miguel','Sandra','Eva']
+
+print('Quien va solo a comer: ')
+print('Quien va solo a cenar: ')
+print('Quien se ha apuntado a un solo evento:')
+print('Quien va a comer y a cenar: ')
+print('Quienes han participado en los eventos:')
+
+
+
+
