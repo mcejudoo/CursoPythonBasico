696dca584ab99a7b41cc1b2f3348eca455490901
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-02-08 11:45:45 +0100

    miercoles_11_45

--- codigo_feb_1/objetos.py
+++ codigo_feb_1/objetos.py
@@ -22,6 +22,11 @@ class Persona:
     def __str__(self):
         return self.nombre + " " + str(self.edad) + " " + str(self.altura)
 
+    def __repr__(self):
+        return str(self)
+
+    def __lt__(self, otro):
+        return self.edad < otro.edad
 
 if __name__ == '__main__':
     obj1 = Persona("Jose", altura=1.77)
@@ -33,3 +38,16 @@ if __name__ == '__main__':
     obj2 = Persona('Ana',33,1.75)
     obj1.hablar(obj2)
     obj2.hablar(obj1)
+    L = [obj1, obj2, Persona('Juan',45,1.88)]
+    for i in L:
+        print(i)
+    print(L)
+    L.sort(key=lambda o : o.nombre)
+    print(L)
+    L.sort()
+    print(L)
+    print(obj1.__dict__)
+    L2 = [o.__dict__ for o in L]
+    print(L2)
+    obj1.telefono = 600334433
+    print(obj1.__dict__)
