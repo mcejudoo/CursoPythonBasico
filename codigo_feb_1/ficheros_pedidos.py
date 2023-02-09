6ea804970e83cce128352b794b4c2318d4ca8b1d
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-02-09 12:29:24 +0100

    jueves_12_29

--- codigo_feb_1/ficheros_pedidos.py
+++ codigo_feb_1/ficheros_pedidos.py
@@ -3,14 +3,25 @@ Repartir todos los pedidos en distintos CSVs. Uno para cada país y guardarlos e
 carpeta ficheros/paises
 """
 
+from datetime import datetime
+
 def procesarPedidos(path):
     """
     Generar un fichero CSV por cada país.
     """
+
+    def getPathLog():
+        t = datetime.now()
+        s = "ficheros/" + t.strftime("%d_%m_%Y_%H_%M_%S") + ".log"
+        return s
+
     f = None
+    fLog = None
     paises = dict()
     cabs = None
     try:
+        pathLog = getPathLog()
+        fLog = open(pathLog, 'w')
         f = open(path, 'r')
         for linea in f:
             linea = linea.rstrip()
@@ -29,23 +40,28 @@ def procesarPedidos(path):
             fPais = None
             try:
                 fPais = open(pathPais, 'w')
-                #if p == 'Suiza': raise Exception('Prueba error con Suiza ...')
+
+                # Simular error en 3 países:
+                if p in ('Suiza','Austria','Venezuela'): raise Exception(f'Error en el pais {p} ...')
+
                 print(f'Generando fichero: {pathPais} con {len(L)} registros ...')
                 contenido = cabs + "\n" + "\n".join(L)
                 fPais.write(contenido)
 
             except Exception as e:
-                print('Error en el fichero: ', pathPais)
-                print(e)
+                t = datetime.now()
+                linea = str(t)+ "\t" + str(e) + "\n"
+                fLog.write(linea)
 
             finally:
-                if fPais: fPais.close()
+                if fPais: fPais.close()                
 
     except Exception as e:
         print(e)
 
     finally:
         if f: f.close()
+        if fLog: fLog.close()
 
 if __name__ == '__main__':
     procesarPedidos('ficheros/Pedidos.txt')        
