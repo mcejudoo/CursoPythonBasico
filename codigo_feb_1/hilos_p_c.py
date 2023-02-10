76753550d1c3d5168e4df01e1748b683d6f8acf6
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-02-10 13:12:20 +0100

    viernes_13_12

--- codigo_feb_1/hilos_p_c.py
+++ codigo_feb_1/hilos_p_c.py
@@ -7,6 +7,7 @@ from time import sleep
 from random import randint
 
 num_muestras = 20
+tam_buffer = 10
 
 class Buffer:
 
@@ -16,7 +17,7 @@ class Buffer:
         self.huecos = Semaphore(size)
         self.iC = 0
         self.iP = 0
-        self.buf = list()
+        self.buf = ['']*size
 
 class Productor(Thread):
 
@@ -26,7 +27,18 @@ class Productor(Thread):
 
     def run(self):
         for i in range(num_muestras):
-            sleep(randint(0,2))
+            letra = chr(randint(65,65+25))
+            print('P: ', letra)
+
+            self.b.huecos.acquire() # Tengo hueco?
+            with self.b.mutex:
+                self.b.buf[self.b.iP] = letra
+                self.b.iP = (self.b.iP + 1) % tam_buffer
+                print('Buffer: ', self.b.buf)
+
+            self.b.items.release() # Avisa al C. que ya tiene un item
+
+            sleep(randint(0,1))
 
 class Consumidor(Thread):
 
@@ -36,10 +48,21 @@ class Consumidor(Thread):
 
     def run(self):
         for i in range(num_muestras):
-            sleep(randint(0,1))
+
+            self.b.items.acquire() # Tengo un item?
+            with self.b.mutex:
+                letra = self.b.buf[self.b.iC]
+                self.b.buf[self.b.iC] = ' '                
+                self.b.iC = (self.b.iC + 1) % tam_buffer
+                print('Buffer: ', self.b.buf)
+
+            self.b.huecos.release()  # Avisar de que ya tiene un hueco
+
+            print('C: ', letra)
+            sleep(randint(1,3))
 
 if __name__ == '__main__':
-    b = Buffer()
+    b = Buffer(tam_buffer)
     p = Productor(b)
     c = Consumidor(b)
 
