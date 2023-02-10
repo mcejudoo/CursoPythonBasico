ba6c0feda39ab3b665a2c50038520f6f8aed592c
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-02-10 14:45:16 +0100

    viernes_14_45

--- codigo_feb_1/pedidos/pruebas_pedidos.py
+++ codigo_feb_1/pedidos/pruebas_pedidos.py
@@ -7,6 +7,8 @@ from unittest import TestCase
 from ficheros_pedidos import procesarPedidos
 import os
 
+path = "ficheros/paises/"
+
 class TestPedidos(TestCase):
     
     def setUp(self):
@@ -15,17 +17,37 @@ class TestPedidos(TestCase):
         """
         procesarPedidos("Pedidos.txt")
 
+    def getNumPaises(self):
+        paises = set()
+        f = None
+        try:
+            f = open("Pedidos.txt", 'r')
+            for linea in f:
+                linea = linea.rstrip()             
+                pais = linea.split(";")[-1]
+                paises.add(pais)
+
+            return len(paises)
+
+        except Exception as e:
+            print(e)
+            return -1
+
+        finally:
+            if f: f.close()
 
     def testNumFicheros(self):
         """Comprobar si se genera el mismo número de ficheros que países distintos tenemos"""
-        pass
+        print(os.listdir(path))
+        numFicheros = len(os.listdir(path))
+        numPaises = self.getNumPaises()
 
+        self.assertEqual(numFicheros, numPaises, msg="El número de ficheros no coincide con el n. de países")
 
     def tearDown(self):
         """
         Vaciar la carpeta de ficheros.
         """
-        path = "ficheros/paises/"
         L = os.listdir(path)
         for f in L:
             os.remove(path+f)
